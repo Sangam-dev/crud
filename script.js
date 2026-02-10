c68349bb65b17c4d972831943448dadfa7c1e5f3
Sangam-dev <parajulis295@gmail.com>
2026-02-10 17:59:03 +0545

    add items

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-   
+    const $inputBox = $('#input');
+    const $listContainer = $('#list-container');
+
+    $('#add-btn').on('click', function () {
+        const value = $inputBox.val().trim();
+
+        if (value === '') {
+            alert('enter value');
+            return;
+        }
+
+        const $li = $('<li></li>');
+        const $textSpan = $('<span></span>', {
+            class: 'item-text',
+            text: value
+        });
+
+        $li.append($textSpan);
+        $listContainer.append($li);
+
+        $inputBox.val('');
+    });
 });
